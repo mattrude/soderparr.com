b06d2b14dc1c96d1074250b667750f34c2f713fc
Matt Rude <matt@mattrude.com>
2017-04-24 17:49:42 -0500

    minor updates

--- site-builder.sh
+++ site-builder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /root/websites/soderparr.com
-bundle exec jekyll build
+cd /root/websites/soderparr.com && \
+bundle exec jekyll build --incremental && \
 rsync -av _site/* odin.mattrude.com:/var/www/soderparr.com/
